ad903a6a2073358c2185a539c1a83939edc4d2e2
Denis Alayza <denis.alayza@gmail.com>
2020-04-27 22:44:46 -0300

    Cadastro Simple angular nodejs mongdb in docker-compose

--- docker1/node-mongo-compose/backend/app.js
+++ docker1/node-mongo-compose/backend/app.js
@@ -12,7 +12,24 @@ mongoose.promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 // Teste
-server.get('/', (req, res, next) => res.send('Backend'))
+// server.get('/', (req, res, next) => res.send('Backend'))
+
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json)
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, require: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({ new: true, runValidators: true })
+
+// Routes
+Client.register(server, '/Clients')
 
 // Start server
 server.listen(3000)
